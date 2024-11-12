ad0861c8b55586f65b31b50fe83340186bfa27e9
zengyong <2595650269@qq.com>
2024-11-12 14:49:08 +0800

    finish: split file to src/include

---
+++ include/func_ptr_pass.h
@@ -0,0 +1,17 @@
+#ifndef __FUNC_PTR_PASS__
+#define __FUNC_PTR_PASS__
+#include <llvm/IR/Instructions.h>
+#include <llvm/IR/LegacyPassManager.h>
+
+using namespace llvm;
+
+struct FuncPtrPass : public ModulePass {
+    static char ID; // Pass identification, replacement for typeid
+    FuncPtrPass()
+        : ModulePass(ID) { }
+    bool can_execuate(llvm::PHINode phiInstr, llvm::Value *operand, llvm::BasicBlock instr_block);
+    void analysis(llvm::CallInst *callInst);
+    bool runOnModule(Module &M) override;
+};
+
+#endif
---
+++ include/utils.h
@@ -0,0 +1,11 @@
+#ifndef __UTILS__
+#define __UTILS__
+
+// 没有这两个不能正常遍历模块
+#include <llvm/Bitcode/BitcodeReader.h>
+#include <llvm/Bitcode/BitcodeWriter.h>
+
+#include <llvm/Support/raw_ostream.h>
+#include <vector>
+
+#endif
