68e63a10dfefdb1eb7dbf6c2a43c4d211f3f3a6a
zengyong <2595650269@qq.com>
2024-11-13 12:49:54 +0800

    fix: test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@ test_dir="assign2-tests/"
 tests=("test00" "test01" "test02" "test03" "test04" "test05" "test06" "test07" 
 "test08" "test09" "test10" "test11" "test12" "test13" "test14" "test15" 
 "test16" "test17" "test18" "test19")
-results=("10 : plus" "22 : plus" "24 : minus, plus" "27 : plus, minus" 
+results=("10 : plus" "22 : plus" "24 : plus, minus" "27 : plus, minus" 
 "10 : plus, minus
 26 : foo
 33 : foo" 
@@ -13,9 +13,9 @@ results=("10 : plus" "22 : plus" "24 : minus, plus" "27 : plus, minus"
 28 : clever
 30 : clever"
 "10 : plus, minus
-26: clever"
+26 : clever"
 "10 : plus, minus
-14 : foo 
+14 : foo
 30 : clever"
 "15 : plus, minus
 19 : foo
@@ -44,8 +44,8 @@ do
     clang -emit-llvm -c -O0 -g3 $test_path -o $bc_path
     ./build/llvmassignment $bc_path 2>tmp.txt
     res=$(cat tmp.txt)
-    # echo $res
-    # echo $expected
+    echo $res
+    echo $expected
     if [ "$expected" = "$res" ];then
         echo "* success $test"
     else 
